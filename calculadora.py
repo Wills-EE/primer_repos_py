cafbdba40d969be23fe94af12ddb2615ac1c33a6
Will Hilario <fredihi1110@gmail.com>
2024-12-22 00:54:05 -0500

    calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+n= float(input('ingrese un numero: '))
+m= float(input('ingrese un numero: '))
+
+suma = n+m
+resta = n-m
+mult = n*m
+div = n/m
+pot = n**m
+
+print(f'{suma}\n{resta}\n{mult}\n{div}\n{pot}')
--- numero.py
+++
@@ -1,7 +0,0 @@
-
-def contar (num):
-    for i in range (num):
-        suma = i + num
-        print(suma)
-    return sum
-print(contar(10))
